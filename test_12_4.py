5886de6e1de6f8213ab8d0c5d110600b6122df69
Александр <alexco@list.ru>
2024-11-20 23:17:57 +0300

    Add files via upload
    Домашнее задание по теме "Логирование"

---
+++ test_12_4.py
@@ -0,0 +1,66 @@
+# Домашнее задание по теме "Логирование"
+
+# импорт необходимых библиотек и файлов
+import unittest
+import logging
+import rt_with_exceptions as rr
+
+# класс Runner, наследуемый от TestCase из модуля unittest
+class RunnerTest1(unittest.TestCase):
+
+    # метод test_walk
+    def test_walk(self):
+        # блок try проверяет код на наличие исключений
+        try:
+            # объект класса Runner с произвольным именем
+            walk_ = rr.Runner('man', -5)
+            # вызов метода walk у этого объекта 10 раз
+            for _ in range(10):
+                walk_.walk()
+            # методом assertEqual сравниваем distance этого объекта со значением 50
+            self.assertEqual(walk_.distance, 50)
+            logging.info('"test_walk" выполнен успешно')
+            # вывод на консоль если все правильно
+            # print ('Test "walk" OK')
+        # блок except выполняется, если в блоке try нашлась ошибка
+        except ValueError:
+            logging.warning('Неверная скорость для Runner')
+
+    # метод test_run
+    def test_run(self):
+        # блок try проверяет код на наличие исключений
+        try:
+            # объект класса Runner с произвольным именем
+            run_ = rr.Runner(5)
+            # вызов метода run у этого объекта 10 раз
+            for _ in range(10):
+                run_.run()
+            # методом assertEqual сравниваем distance этого объекта со значением 100
+            self.assertEqual(run_.distance, 100)
+            logging.info('"test_run" выполнен успешно')
+            # вывод на консоль если все правильно
+            # print('Test "run" OK')
+        # блок except выполняется, если в блоке try нашлась ошибка
+        except TypeError:
+            logging.warning('Неверный тип данных для объекта Runner')
+
+
+    # # метод test_challenge
+    # def test_challenge(self):
+    #     # создаются 2 объекта класса Runner с произвольными именами
+    #     challenge1 = rr.Runner('man_R', 10)
+    #     challenge2 = rr.Runner('man_W', 5)
+    #     # 10 раз у объектов вызываются методы run и walk соответственно
+    #     for _ in range(10):
+    #         challenge1.run()
+    #         challenge2.walk()
+    #     # метод assertNotEqual, чтобы убедится в неравенстве результатов
+    #     self.assertNotEqual(challenge1.distance, challenge2.distance)
+    #     # вывод на консоль если все правильно
+    #     print('Test "challenge" OK')
+
+
+logging.basicConfig(level=logging.INFO, filemode='w',
+                    filename='runner_tests.log', encoding='utf-8',
+                    format='%(asctime)s | %(levelname)s | %(message)s')
+
